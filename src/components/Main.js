688498b4bf8ae95b00631b10815b9ddfc2d00888
taotaoabc <289217113@qq.com>
2017-07-12 16:07:24 +0800

    20170712

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,53 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+var imageDatas = require('../data/imageData.json');
+
+imageDatas = (function(imageDatas){
+	for (var i = 0,j=imageDatas.length ;i < j; i++) {
+		var singleID = imageDatas[i];
+		singleID.imgUrl =
+		require('../images/'+imageDatas[i].filename);
+		imageDatas[i] =singleID;
+	}
+	return imageDatas;
+})(imageDatas);
+
+class imgFigure extends React.Component{
+	render(){
+		return(
+			<figure>
+				<img src={this.props.data.imgUrl}
+				alt={this.props.data.title}/>
+				<figcaption>
+					<h2>{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+			);
+	}
+}
+
+
+//let yeomanImage = require('../images/Tulips.jpg');
 
 class AppComponent extends React.Component {
   render() {
+  	var controllerUnits = [],
+  		imgFigures = [];
+
+  	imageDatas.forEach(function(value){
+  		imgFigures.push(<imgFigure key={value.filename} data={value}/>);
+  	});
+
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+    	<section className="stage">
+    		<section className='imgSec'>
+    			{imgFigures}
+    		</section>
+    		<nav className='ctrl-nav'>
+    			{controllerUnits}
+    		</nav>
+    	</section>
     );
   }
 }
